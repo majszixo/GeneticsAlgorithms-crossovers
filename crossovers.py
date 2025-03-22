d86b142c15c90e6caaa62842b2b4918964aab2c2
Agnieszka Majchrzak <80783186+majszixo@users.noreply.github.com>
2025-03-22 10:09:00 +0100

    Update crossovers.py
    kPointCrossover

--- crossovers.py
+++ crossovers.py
@@ -4,6 +4,7 @@ import random
 
 class Animal:
     def __init__(self, species, offspring, survivalRate, categoryA, categoryBplus, categoryB, categoryC):
+        self.species = species
         self.offspring = offspring
         self.survivalRate = survivalRate
         self.categoryA = categoryA
@@ -13,7 +14,7 @@ class Animal:
         self.fitness = self.evaluateFitness()
 
     def evaluateFitness(self):
-        return self.offspring * (self.survivalrate / 100) * (self.categoryA + self.categoryBplus + self.categoryB + self.categoryC) / 100
+        return self.offspring * (self.survivalRate / 100) * (self.categoryA + self.categoryBplus + self.categoryB + self.categoryC) / 100
     
     def __repr__(self):
         return f"{self.species}: offspring = {self.offspring}, survival rate = {self.survival_rate}%, fitness = {self.fitness:.2f}"
@@ -25,6 +26,20 @@ def onePointCrossover(parent_1, parent_2):
 
     new_attributes = parent_1_attrs[:randomPointCrossover] + parent_2_attrs[randomPointCrossover:]
     return Animal(parent_1.species, *new_attributes)
+
+def kPointCrossover(parent_1, parent_2, k):
+    points = sorted(random.sample(range(1, 6), k))
+    attrs = ["offspring", "survivalRate", "categoryA", "categoryBplus", "categoryB", "categoryC"]
+    useParent_1 = True
+    new_attributes = []
+    for i in range(0,6):
+        if i in points:
+            use_parent_1 = not use_parent_1
+        attr_value = getattr(parent_1 if use_parent_1 else parent_2, attrs[i])
+        new_attributes.append(attr_value)
+
+    return Animal(parent_1.species, *new_attributes)
+
 
     
 
